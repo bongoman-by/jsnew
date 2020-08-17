92d46062083f1d782dcd9e4dfebe1a09aebb4a38
Sergey <bongoman@tut.by>
2020-08-17 21:46:57 +0300

    Lesson 012

--- js/script.js
+++ js/script.js
@@ -1,4 +1,22 @@
-const category = 'toys';
 
-// console.log('https://url.com/' + category);
-console.log(`https://url.com/${category}/5`);
+const numberOfFilms = +prompt("Сколько фильмов вы уже просмотрели?", 0);
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+let i = 0;
+while (i < 2) { // выводит 0, затем 1, затем 2
+    const a = prompt("Один из последних просмотренных фильмов?"),
+          b = +prompt(`Оценка фильма ${a} (1-5)?`);
+
+          personalMovieDB.movies[a] = b;
+
+  i++;
+}
+
+console.log(personalMovieDB);
