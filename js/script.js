16fa85ccfe334fdcda594fe0c016b5ae843a4b53
Sergey <bongoman@tut.by>
2020-08-22 17:14:22 +0300

    lesson 18

--- js/script.js
+++ js/script.js
@@ -5,25 +5,13 @@ switch (numberOfLesson) {
 
         console.log('015 Практика, ч2. Применяем условия и циклы');
 
-        const numberOfFilms = +prompt("Сколько фильмов вы уже просмотрели?", 0);
+        let numberOfFilms = start(null);
 
-        const personalMovieDB = {
-            count: numberOfFilms,
-            movies: {},
-            actors: {},
-            genres: [],
-            privat: false
-        };
+        let personalMovieDB = rememberMyFilms(numberOfFilms);
 
-        if (personalMovieDB.count < 10) {
-            alert('Просмотрено довольно мало фильмов!');
-        } else if (personalMovieDB.count > 9 && personalMovieDB.count < 31) {
-            alert('Вы классический зритель!');
-        } else if (personalMovieDB.count > 30) {
-            alert('Вы киноман!');
-        } else {
-            alert('Произошла ошибка!');
-        }
+        console.log(numberOfFilms);
+
+        detectPersonalLevel(personalMovieDB);
 
         if (numberOfFilms > 0 && numberOfFilms < 5) {
 
@@ -33,7 +21,7 @@ switch (numberOfLesson) {
                 const a = prompt(`Название фильма № ${i}?`),
                     b = +prompt(`Оценка фильма "${a}"(1-5)?`);
 
-                if (a == null||a.length== 0||a.length>50||b==null||b==0||b>5) {
+                if (a == null || a.length == 0 || a.length > 50 || b == null || b == 0 || b > 5) {
                     continue;
                 }
 
@@ -44,15 +32,17 @@ switch (numberOfLesson) {
 
         }
 
-        console.log(personalMovieDB);
+        writeYourGenres(personalMovieDB);
+
+        showMyDB(personalMovieDB);
 
         break;
     case 16:
-         
-    console.log('016 Функции, стрелочные ф-ции (ES6)');
-  
-    let ask = (question, yes, no) => (confirm(question)) ? yes() : no();
-    
+
+        console.log('016 Функции, стрелочные ф-ции (ES6)');
+
+        let ask = (question, yes, no) => (confirm(question)) ? yes() : no();
+
         ask(
             "Вы согласны?",
             () => alert("Вы согласились."),
@@ -66,4 +56,62 @@ switch (numberOfLesson) {
         break;
     default:
         console.log('Занятия с таким номером не найдено!');
+}
+
+function start(numberOfFilms) {
+
+    while (numberOfFilms == null || numberOfFilms == 0 || isNaN(numberOfFilms)) {
+
+        return +prompt("Сколько фильмов вы уже просмотрели?");
+
+    }
+}
+
+function rememberMyFilms(numberOfFilms) {
+
+    return {
+        count: numberOfFilms,
+        movies: {},
+        actors: {},
+        genres: [],
+        privat: false
+    };
+
+}
+
+function detectPersonalLevel(personalMovieDB) {
+
+    if (personalMovieDB.count < 10) {
+        alert('Просмотрено довольно мало фильмов!');
+    } else if (personalMovieDB.count > 9 && personalMovieDB.count < 31) {
+        alert('Вы классический зритель!');
+    } else if (personalMovieDB.count > 30) {
+        alert('Вы киноман!');
+    } else {
+        alert('Произошла ошибка!');
+    }
+}
+
+function writeYourGenres(personalMovieDB) {
+
+    for (let i = 1; i <= 3; i++) {
+
+        const a = prompt(`Ваш любимый жанр под № ${i}?`);
+
+        if (a == null || a.length == 0) {
+            i--;
+            continue;
+        }
+
+        personalMovieDB.genres[i - 1] = a;
+
+    }
+
+}
+
+function showMyDB(personalMovieDB) {
+
+    if (personalMovieDB.privat == false) {
+        console.log(personalMovieDB);
+    }
 }
