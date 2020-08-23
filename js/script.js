375688ef6e58ed158b55dc37fcad23fdae21b4ac
Sergey <bongoman@tut.by>
2020-08-23 21:49:47 +0300

    030 Практика. Задание на отработку действий со страницей

--- js/script.js
+++ js/script.js
@@ -1,22 +1,42 @@
+/* Задания на урок:
+
+1) Удалить все рекламные блоки со страницы (правая часть сайта)
+
+2) Изменить жанр фильма, поменять "комедия" на "драма"
+
+3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
+Реализовать только при помощи JS
+
+4) Список фильмов на странице сформировать на основании данных из этого JS файла.
+Отсортировать их по алфавиту 
+
+5) Добавить нумерацию выведенных фильмов */
+
 'use strict';
 
-const box = document.getElementById('box'),
-      btns = document.body.querySelectorAll('button');
+const movieDB = {
+    movies: [
+        "Логан",
+        "Лига справедливости",
+        "Ла-ла лэнд",
+        "Одержимость",
+        "Джокер"
+    ]
+};
 
-box.style.backgroundColor = 'grey';
-box.style.width = '100px';
+const promoAdv = document.querySelector('.promo__adv');
+promoAdv.remove();
 
-console.log(btns);
+const promoGenre = document.querySelector('.promo__genre');
+promoGenre.textContent = 'ДРАМА';
 
-btns.forEach(element => {
-    element.style.borderRadius = '70%';
-    element.style.backgroundColor = 'green';
+const promoBG = document.querySelector('.promo__bg');
+console.log(promoBG.backgroundImage);
+promoBG.style.backgroundImage = "url('../img/bg.jpg')";
+
+const promoList = document.querySelectorAll('.promo__interactive-item');
+movieDB.movies.sort();
+movieDB.movies.forEach((element, i) => {
+    promoList[i].textContent = `${i + 1}. ${element}`;
 });
 
-const div = document.createElement('div');
-
-div.classList.add('black');
-
-btns[0].before(div);
-
-div.innerHTML = "<h1>Hello</h1>";
