ddf4e72e10b537e4aa8804bfce218b0ea08d80e0
Sergey <bongoman@tut.by>
2020-08-29 14:41:48 +0300

    033 Практика. Используем события на странице проекта

--- js/script.js
+++ js/script.js
@@ -1,35 +1,97 @@
-const over = document.querySelectorAll('.over')[0],
-btn = over.querySelector('button'),
-link = document.querySelector('a'),
-btns = document.querySelectorAll('button');
+'use strict';
 
-// btn.addEventListener('click', () =>{
-//     alert('Click');
-// });
+document.addEventListener('DOMContentLoaded', () => {
 
-// btn.addEventListener('click', () =>{
-//     alert('Second Click');
-// });
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    };
 
-let i = 0;
-const deleteButton = (event) => {
-    // event.target.remove();
-    console.log(event.currentTarget);
-    i++;
-    if (i == 2) {
-        btn.removeEventListener('click', deleteButton);
-        over.removeEventListener('click', deleteButton);
+    const maxLength = 21,
+        film = 'Полет над гнездом кукушки',
+        advlist = document.querySelectorAll(".promo__adv img"),
+        genre = document.querySelector(".promo__genre"),
+        poster = document.querySelector(".promo__bg"),
+        movielist = document.querySelector(".promo__interactive-list"),
+        addForm = document.querySelector('form.add'),
+        addInput = addForm.querySelector('.adding__input'),
+        checkbox = addForm.querySelector('[type="checkbox"]');
+
+    function checkFilm(newFilm, maxLength) {
+
+        if (newFilm.length > maxLength) {
+            return `${newFilm.slice(0, maxLength)}...`;
+        }
+
+        return newFilm;
+
     }
-};
 
-btn.addEventListener('click', deleteButton);
-over.addEventListener('click', deleteButton);
+    function createMovieList(films, parent) {
 
-link.addEventListener('click', function(event) {
-    event.preventDefault();
+        parent.innerHTML = "";
+
+        films.sort();
+
+        films.forEach((film, i) => {
+            parent.innerHTML += `<li class="promo__interactive-item">${i + 1}. ${film.toUpperCase()}
+            <div class="delete"></div>
+        </li>`;
+        });
+
+        document.querySelectorAll('.delete').forEach((btn, i) => {
+            btn.addEventListener('click', () => {
+                
+                btn.parentElement.remove();
+                films.splice(i, 1);
+                createMovieList(films, parent);
+
+            });
+        });
+
+    }
+
+    const deleteAdv = (arr) => {
+
+        arr.forEach((item) => {
+            item.remove();
+        });
+
+    };
+
+    const makeChanges = () => {
+        
+        genre.textContent = 'DRAMA';
+          poster.style.backgroundImage = 'url("img/bg.jpg")';
+    };
+
+    addForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        let newFilm = addInput.value,
+            favorite = checkbox.checked;
+
+        if (newFilm) {
+
+            movieDB.movies.push(checkFilm(newFilm, maxLength));
+            createMovieList(movieDB.movies, movielist);
+            event.target.reset();
+
+            if (favorite) {
+                console.log('Добавляем новый фильм!');
+            }
+
+        }
+
+    });
+
+    createMovieList(movieDB.movies, movielist);
+    deleteAdv(advlist);
+    makeChanges();
+
 });
-
-btns.forEach(btn => {
-    btn.addEventListener('click', deleteButton);
-});
- 
