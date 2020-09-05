38148622e04bd54b98d04b0d4ce651ddf65f1713
Sergey <bongoman@tut.by>
2020-09-05 16:33:54 +0300

    056 Fetch API

--- js/script.js
+++ js/script.js
@@ -112,7 +112,10 @@ window.addEventListener('DOMContentLoaded', () => {
         modal.classList.add(params[0]);
         modal.classList.remove(params[1]);
         document.body.style.overflow = [2];
-        clearInterval(modalTimeout);
+        if (params[0] === 'show') {
+            clearInterval(modalTimeout);
+        }
+
     }
 
     modalTriggers.forEach((elem) => {
@@ -126,7 +129,7 @@ window.addEventListener('DOMContentLoaded', () => {
     });
 
     modal.addEventListener('click', (e) => {
-        if (e.target === modal) {
+        if (e.target === modal || e.target.getAttribute('data-close') === '') {
             actionModal(['hide', 'show', '']);
         }
     });
@@ -200,7 +203,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     const forms = document.querySelectorAll('form'),
         message = {
-            loading: 'Загрузка',
+            loading: '/img/form/054 spinner.svg',
             success: 'Спасибо! Скоро мы с вами свяжемся',
             failure: 'Что-то пошло не так..'
         };
@@ -231,34 +234,108 @@ window.addEventListener('DOMContentLoaded', () => {
         form.addEventListener('submit', (e) => {
             e.preventDefault();
 
-            const statusMessage = document.createElement('div');
-            statusMessage.classList.add('status');
-            statusMessage.textContent = message.loading;
-            form.append(statusMessage);
+            const statusMessage = document.createElement('img');
+            statusMessage.src = message.loading;
+            statusMessage.style.cssText = `
+            display: block;
+            margin: 0 auto`;
+            form.insertAdjacentElement('afterend', statusMessage);
 
-            const request = new XMLHttpRequest(),
-                method = 'POST',
-                url = 'server.php',
-                async = true,
-                    login = 'Sergey',
-                    pass = '1111';
+            // const request = new XMLHttpRequest(),
+            //     method = 'POST',
+            //     url = 'server.php',
+            //     async = true,
+            //         login = 'Sergey',
+            //         pass = '1111';
 
-            request.open(method, url, async, login, pass);
-            sendServer(request, form);
+            // request.open(method, url, async, login, pass);
+            // sendServer(request, form);
 
-            request.addEventListener('load', () => {
+            // request.addEventListener('load', () => {
 
-                if (request.status === 200) {
-                    console.log(request.response);
-                    statusMessage.textContent = message.success;
-                    form.reset();
-                    setTimeout(() => {
-                        statusMessage.remove();
-                    }, 2000);
-                } else {
-                    statusMessage.textContent = message.failure;
-                }
+            //     if (request.status === 200) {
+            //         form.reset();
+            //         console.log(request.response);
+            //         statusMessage.remove();
+            //         showThanksModal(message.success);
+            //     } else {
+            //         form.reset();
+            //         showThanksModal(message.failure);
+            //     }
+            // });
+
+            const formData = new FormData(form);
+            const object = {};
+            formData.forEach(function (value, key) {
+                object[key] = value;
             });
+            const json = JSON.stringify(object);
+
+            fetch('server.php', {
+                method: "POST",
+                body: json,
+                headers: {
+                    'Content-type': 'application/json'
+                }
+            })
+            .then(data => data.text())
+            .then(data => {
+                form.reset();
+                console.log(data.response);
+                statusMessage.remove();
+                form.reset();
+                showThanksModal(message.success);
+            }).catch(() => {
+                form.reset();
+                showThanksModal(message.failure);
+
+            }).finally(() => {
+                form.reset();
+            });
+
         });
     }
+
+    // nice modal
+
+    function showThanksModal(message) {
+        const prevModalDialog = document.querySelector('.modal__dialog');
+        prevModalDialog.classList.add('hide');
+
+        actionModal(['hide', 'show', '']);
+
+        const thanksModal = document.createElement('div');
+        thanksModal.classList.add('modal__dialog');
+        thanksModal.innerHTML = `
+        <div class="modal__content">
+            <div data-close class="modal__close">x</div>
+            <div class="modal__title">${message}</div>
+        </div>`;
+        document.querySelector('.modal').append(thanksModal);
+        actionModal(['show', 'hide', '']);
+
+        setTimeout(() => {
+            thanksModal.remove();
+            prevModalDialog.classList.add('show');
+            prevModalDialog.classList.remove('hide');
+            actionModal(['hide', 'show', '']);
+        }, 4000);
+
+    }
+
+    //API
+
+    //     fetch('https://jsonplaceholder.typicode.com/todos/1')
+    //   .then(response => response.json()) 
+    //   .then(json => console.log(json));
+
+    // fetch('https://jsonplaceholder.typicode.com/posts', {
+    //     method: "POST",
+    //     body: JSON.stringify({name: 'Sergey'}),
+    //     headers: {
+    //         'Content-type': 'application/json'
+    //     }
+    // })
+    //    .then(response => response.json())
+    //    .then(json => console.log(json));
 });
--- server.php
+++ server.php
@@ -1,13 +1,5 @@
 <?php
-if(isset($_POST['login']) && isset($_POST['password'])){
- 
-    $login=$_POST['login'];
-    $password = $_POST['password'];
-    echo "Ваш логин: $login и пароль: $password";
-}   
-    //$_POST = json_decode(file_get_contens("php://input"), true);
+
     $postData = file_get_contents('php://input');
     $data = json_decode($postData, true);
-
-	print_r($data);
-?>
+	echo $data;
